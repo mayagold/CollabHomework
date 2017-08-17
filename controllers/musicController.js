b194a108bc9b0ae153562793fef1fd228e939ae4
Mike Skelton <mskelton19@Mikes-MBP.home>
2017-08-17 14:13:34 -0400

    updated models and controller

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -1,2 +1,31 @@
+const express = require('express');
+const router = express();
+const music = require('../models/music.js');
 
-// router 
+// router
+
+router.get('/', (req, res) => {
+  music.find({}, (err, foundMusic) => {
+    res.json(foundMusic);
+  })
+})
+
+router.post('/', (req, res) => {
+  music.create(req.body, (err, createdMusic) => {
+    res.json(createdMusic);
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  music.findByIdAndRemove(req.params.id, (err, deletedMusic) => {
+    res.json(deletedMusic)
+  })
+})
+
+router.put('/:id', (req, res) => {
+  music.findByIdAndUpdate(req.params.id, req.body, (err, updateMusic) => {
+    res.json(updateMusic)
+  })
+})
+
+module.exports = router;
--- models/music.js
+++ models/music.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 
 // model
+
+const musicSchema = mongoose.Schema({
+  Artist: String,
+  Album: String,
+  Year: String,
+  Picture: String
+})
+
+const music = mongoose.model('Music', musicSchema);
+
+module.exports = music;
